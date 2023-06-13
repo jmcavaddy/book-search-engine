8cc4c79e19388f8a05853d166307f5882b3c24d0
jmcavaddy <jakemcavaddy@gmail.com>
2023-06-13 09:35:36 -0400

    Trying to fix heroku

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
